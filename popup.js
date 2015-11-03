815b7b2a76c95c068d9f8381405d2f57ebf11d34
Grady Rogers <gradyrogers@Gradys-MacBook-Pro.local>
2015-11-02 21:49:22 -0500

    working extension

--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 
-  articleTempl = _.template(templates.article);
-  articles =[];
 
   var page = {
     init: function() {
@@ -12,16 +10,15 @@ $(document).ready(function(){
         url: 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7?api-key=1b49c5883656e73007430a84af82844a:6:73362387',
         method: 'GET',
         success: function(data) {
-          for (i = 0; i < articles.length; i++) {
-            title = articles[i].title;
-            pic = articles[i].media[0]['media-metadata'][0].url;
-            description = articles[i].abstract;
-            link = articles[i].url;
-            //date = articles[i].published_date;
+          for (i = 0; i < data.results.length; i++) {
+            title = data.results[i].title;
+            backgroundPic = data.results[i].media[0]['media-metadata'][0].url;
+            description = data.results[i].abstract;
+            redirect = data.results[i].url;
 
              $('.display').append("<li style='background-image: url(" +  backgroundPic + ")'>" +
-                       "<a target='_blank' href='" + redirect + "'<h3>" + "headline" + "</h3></a>" +
-                       "<br /><p class='block'>" + "text" + "</p></li>");
+                       "<a target='_blank' href='" + redirect + "'<h3>" + title + "</h3></a>" +
+                       "<br /><p class='block'>" + description + "</p></li>");
           }
         }
       });
